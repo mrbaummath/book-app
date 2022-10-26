924004df2d93d98721093ee300839e86d440c8ad
mrbaummath <baum1286@gmail.com>
2022-10-26 15:10:32 -0400

    Home page will now index if user is logged in and otherwise asks user to login. Removed 'see all books' link in nav since the landing page for logged in users is now the index

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,19 @@
+import React from 'react'
+import { Navigate } from 'react-router-dom'
+
+
 const Home = (props) => {
-	// const { msgAlert, user } = props
-	console.log('props in home', props)
+	const { user } = props
+	if (user) {
+		return(
+			<Navigate to='/books' />
+		)
+	}
 
 	return (
 		<>
 			<h2>Welcome to The Library!</h2>
+			<p>Please sign up or login to see all books</p>
 		</>
 	)
 }
--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -19,11 +19,6 @@ const authenticatedOptions = (
 			</Link>
 		</Nav.Item>
 		<Nav.Item>
-			<Link to='books/' style={linkStyle}>
-				See all Books
-			</Link>
-		</Nav.Item>
-		<Nav.Item>
 			<Link to='books/create' style={linkStyle}>
 				Create a Book
 			</Link>
